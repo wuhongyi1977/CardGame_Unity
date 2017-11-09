29fb225fe4a6a711576183cf359df8246808cbf0
arashihojo <33140376+arashihojo@users.noreply.github.com>
2017-11-10 01:01:46 +0900

    arashi_hojo
    Card & AttackCard class定義を追加
    csv読み込みは仕様が未定なのでまだです

---
+++ CardGame/Assets/Scripts/AttackCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCard : Card {
+
+    protected int atk;
+
+    public AttackCard(){
+        this.atk = 0;
+    }
+
+    public AttackCard(string name, string id, string explain, string illust, int atk){
+        this.card_name = name;
+        this.card_id = id;
+        this.card_explain = explain;
+        this.card_color = Color.red;
+        this.card_illust = illust;
+        this.atk = atk;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- CardGame/Assets/Scripts/Card.cs
+++ CardGame/Assets/Scripts/Card.cs
@@ -2,7 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Card : MonoBehaviour {
+
+    protected string card_name;
+    protected string card_id;
+    protected string card_explain;
+    protected Color card_color;
+    protected string card_illust;
+    //イラストはとりあえずstringにしたけどどう扱う？ID判別でspriteの集まりから呼び出し？
+
+    public Card() {
+        this.card_name = "";
+        this.card_id = "";
+        this.card_explain = "";
+        this.card_color = Color.white;
+        this.card_illust = "";
+    }
+
+    public Card(string name, string id, string explain, Color color, string illust){
+        this.card_name = name;
+        this.card_id = id;
+        this.card_explain = explain;
+        this.card_color = color;
+        this.card_illust = illust;
+    }
+   
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +38,5 @@ public class Card : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
