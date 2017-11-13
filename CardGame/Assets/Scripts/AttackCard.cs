a34776b9829fa2a8fc87f109e14d51c4a5a69f34
Mocchaso <saka.ksftn2718@gmail.com>
2017-11-14 01:29:57 +0900

    Defenseクラスを作成しました(Attackクラスの形に準拠)
    他に、
    ・def.csvの中身を1行分追加、Resources/CSVに移動
    ・.gitignoreファイルの内容を少し追記(https://github.com/github/gitignore/blob/master/Unity.gitignoreを参照)

---
+++ CardGame/Assets/Scripts/AttackCard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AttackCard : Card {
+
+    protected int atk1;
+    protected int atk2;
+    protected int atk3;
+
+    //csv関連(using System & using System.IOも追加)
+    private string csvFilePath="atk";
+    private string[] csvDatas = new string[] {};
+
+    public AttackCard(){
+        this.atk1 = 0;
+        this.atk2 = 0;
+        this.atk3 = 0;
+    }
+
+    public AttackCard(string name, string id, string explain, string illust, int atk1, int atk2, int atk3){
+        this.card_name = name;
+        this.card_id = id;
+        this.card_explain = explain;
+        this.card_color = Color.red;
+        this.card_illust = illust;
+        this.atk1 = atk1;
+        this.atk2 = atk2;
+        this.atk3 = atk3;
+    }
+
+    //IDに対応した数字からcsvを読み込むコンストラクタ
+    public AttackCard(int num){
+        CsvReader csvReader = new CsvReader();
+        csvDatas = csvReader.Readcsv(csvFilePath,num);
+
+        this.card_id = csvDatas[0];
+        this.card_name = csvDatas[1];
+        this.card_explain = csvDatas[2];
+        this.card_color = Color.red;
+        this.card_illust = csvDatas[3];
+        this.atk1 = Convert.ToInt32(csvDatas[4]);
+        this.atk2 = Convert.ToInt32(csvDatas[5]);
+        this.atk3 = Convert.ToInt32(csvDatas[6]);
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ CardGame/Assets/Scripts/DefenseCard.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DefenseCard : Card
+{
+
+    protected int def1;
+    protected int def2;
+    protected int def3;
+
+    //csv関連(using System & using System.IOも追加)
+    private string csvFilePath = "def";
+    private string[] csvDatas = new string[] { };
+
+    public DefenseCard()
+    {
+        this.def1 = 0;
+        this.def2 = 0;
+        this.def3 = 0;
+    }
+
+    public DefenseCard(string name, string id, string explain, string illust, int def1, int def2, int def3)
+    {
+        this.card_name = name;
+        this.card_id = id;
+        this.card_explain = explain;
+        this.card_color = Color.blue;
+        this.card_illust = illust;
+        this.def1 = def1;
+        this.def2 = def2;
+        this.def3 = def3;
+    }
+
+    //IDに対応した数字からcsvを読み込むコンストラクタ
+    public DefenseCard(int num)
+    {
+        CsvReader csvReader = new CsvReader();
+        csvDatas = csvReader.Readcsv(csvFilePath, num);
+
+        this.card_id = csvDatas[0];
+        this.card_name = csvDatas[1];
+        this.card_explain = csvDatas[2];
+        this.card_color = Color.blue;
+        this.card_illust = csvDatas[3];
+        this.def1 = Convert.ToInt32(csvDatas[4]);
+        this.def2 = Convert.ToInt32(csvDatas[5]);
+        this.def3 = Convert.ToInt32(csvDatas[6]);
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
