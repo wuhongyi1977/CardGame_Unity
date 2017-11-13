f7874e884c893532f6575225edd3502cd623ab0e
arashihojo <33140376+arashihojo@users.noreply.github.com>
2017-11-13 23:15:16 +0900

    Attack値の追加など
    Attack値の追加
    コンストラクタ書き換え
    csvタイトル追加

--- CardGame/Assets/Scripts/AttackCard.cs
+++ CardGame/Assets/Scripts/AttackCard.cs
@@ -6,24 +6,29 @@ using UnityEngine;
 
 public class AttackCard : Card {
 
-    protected int atk;
+    protected int atk1;
+    protected int atk2;
+    protected int atk3;
 
     //csv関連(using System & using System.IOも追加)
     private string csvFilePath="atk";
-    private TextAsset csvFile;
     private string[] csvDatas = new string[] {};
 
     public AttackCard(){
-        this.atk = 0;
+        this.atk1 = 0;
+        this.atk2 = 0;
+        this.atk3 = 0;
     }
 
-    public AttackCard(string name, string id, string explain, string illust, int atk){
+    public AttackCard(string name, string id, string explain, string illust, int atk1, int atk2, int atk3){
         this.card_name = name;
         this.card_id = id;
         this.card_explain = explain;
         this.card_color = Color.red;
         this.card_illust = illust;
-        this.atk = atk;
+        this.atk1 = atk1;
+        this.atk2 = atk2;
+        this.atk3 = atk3;
     }
 
     //IDに対応した数字からcsvを読み込むコンストラクタ
@@ -36,7 +41,9 @@ public class AttackCard : Card {
         this.card_explain = csvDatas[2];
         this.card_color = Color.red;
         this.card_illust = csvDatas[3];
-        this.atk = Convert.ToInt32(csvDatas[4]);
+        this.atk1 = Convert.ToInt32(csvDatas[4]);
+        this.atk2 = Convert.ToInt32(csvDatas[5]);
+        this.atk3 = Convert.ToInt32(csvDatas[6]);
 
     }
 
