cb997c138b1cbc1039ca183562970fe7c457d2d8
Mocchaso <saka.ksftn2718@gmail.com>
2017-11-20 14:34:34 +0900

    カード関係のクラスの修正+α
    ・AttackCard.csとDefenseCard.csのそれぞれの2つのコンストラクタで、属性によってパワーの設定が変わるようにした。
    ・Card.csのcard_explainをcard_describeに変更した。
    ・CSVファイルを小文字版だけにした(AttackCard.csとDefenseCard.csの変数csvFilePathが小文字なので、小文字版がオリジナルだと思われます)。

--- CardGame/Assets/Scripts/Card.cs
+++ CardGame/Assets/Scripts/Card.cs
@@ -8,37 +8,41 @@ public class Card : MonoBehaviour {
     protected string card_id;
     protected string card_name;
     protected string card_attribute;
-    protected string card_explain;
+    protected string card_describe;
     protected Color card_color;
     protected string card_illust;
     //イラストはとりあえずstringにしたけどどう扱う？ID判別でspriteの集まりから呼び出し？
 
-    public Card() {
+    public Card()
+    {
         this.card_id = "";
         this.card_name = "";
         this.card_attribute = "";
-        this.card_explain = "";
+        this.card_describe = "";
         this.card_color = Color.white;
         this.card_illust = "";
     }
 
-    public Card(string id, string name, string attribute, string explain, Color color, string illust){
+    public Card(string id, string name, string attribute, string describe, Color color, string illust)
+    {
         this.card_id = id;
         this.card_name = name;
         this.card_attribute = attribute;
-        this.card_explain = explain;
+        this.card_describe = describe;
         this.card_color = color;
         this.card_illust = illust;
     }
    
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
