7f11f7a14858222ea88aadc9ced32c830412c0aa
mossannhakase <33398758+mossannhakase@users.noreply.github.com>
2017-12-19 14:57:08 +0900

    Add files via upload
    仮置きですまだまだ修正します

---
+++ CardGame/Assets/Scripts/turnchange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+//using UnityEngine;
+
+public class TurnChange{
+    public int a;
+    //”½‘Îˆ—
+    public string reverse(){
+        return reverce;
+    }
+    //”½“]ˆ—
+    public string turn(){
+        if(int a==0){
+            int a=1;
+        }else{
+            int a=0;
+        }
+        return int a;
+    }
+}
