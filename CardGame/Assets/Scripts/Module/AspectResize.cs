93367406ec3f3e6449a74b08a76041f691394b8b
Mocchaso <saka.ksftn2718@gmail.com>
2017-12-26 20:54:03 +0900

    GameManager.csの作成等
    ・GameManager.csを作成、Game.csを削除した。
    ☆シーンを複数作り、それぞれのシーンでGameObjectを作る。それにTitleManager.csのような管理スクリプトをアタッチする。
    ・CardManager.csの名前変更の「"card"+i」を、「"card"+(i+1)」に変えた。
    ・博士作のTurnChange.csをGameManager.csに盛り込んだ。クラス名を、TurnChangeからTurnManagerに変えた。

---
+++ CardGame/Assets/Scripts/Module/AspectResize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectResize : MonoBehaviour {
+
+    public float hei;
+    public float wid;
+
+	public Vector3 Resized() {
+        //スクリーンのサイズの比率を求める
+        float ratio = (float)Screen.width / (float)Screen.height;
+        //比率に合わせて横幅を調整（ベース画像の比率が16:10）
+        hei = 1f;
+        wid = ratio / 1.6f;
+
+        //スケールを求める
+        Vector3 scale = new Vector3(wid, hei, 1.0f);
+        return scale;
+    }
+
+    public Vector3 Reposition(float bx, float by) {
+        //スクリーンのサイズの比率を求める
+        float ratio = (float)Screen.width / (float)Screen.height;
+        //比率に合わせて横幅を調整（ベース画像の比率が16:10）
+        hei = 1f;
+        wid = ratio / 1.6f;
+        //スケールを求める
+        Vector3 pos = new Vector3(wid * bx, hei * by, 0.0f);
+        return pos;
+    }
+}
---
+++ CardGame/Assets/Scripts/Scene/Main/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Constants {
+    public const int CARD_MAX = 20;
+}
+
+public class TurnManager {
+    public int turn;
+
+    public TurnManager(int t)
+    {
+        this.turn = t;
+    }
+
+    // もう一方のターンを取得する
+    public int getNextTurn()
+    {
+        if (turn == 0) return 1;
+        else if (turn == 1) return 0;
+        return -1;
+    }
+    // ターンを切り替える
+    public void changeTurn()
+    {
+        if (turn == 0){
+            turn = 1;
+        } else if (turn == 1)
+        {
+            turn = 0;
+        }
+    }
+}
+
+public class GameManager : MonoBehaviour {
+    private int turn;
+    private GameObject[] players;
+    private TurnManager tm;
+
+	// Use this for initialization
+	void Start () {
+        turn = 0;
+        players = new GameObject[2];
+        players[0] = transform.FindChild("Player").gameObject;
+        players[1] = transform.FindChild("Enemy").gameObject;
+        tm = new TurnManager(turn);
+        StartCoroutine(GameLoop());
+	}
+
+    // 最初に呼び出され、ゲームの各段階で順に実行される
+    private IEnumerator GameLoop()
+    {
+        // ドロー
+        yield return StartCoroutine(RoundDraw());
+        // 攻撃側から守備側への攻撃
+        yield return StartCoroutine(RoundAttack());
+        // ダメージ計算
+        yield return StartCoroutine(RoundCalcDamage());
+        // 墓地行き判定
+        yield return StartCoroutine(RoundTomb());
+        // ターン切り替え
+        yield return StartCoroutine(RoundTurnChange());
+
+        if (isFinished())
+        {
+            // ゲーム終了判定がされれば、結果シーンに遷移する
+            SceneManager.LoadScene("Result");
+        } else
+        {
+            // ゲーム終了判定がされなければ、コルーチンGameLoopをリスタートする
+            // 現時点で行っているGameLoopは終了する
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    private IEnumerator RoundDraw()
+    {
+        GameObject deck = players[turn].transform.FindChild("Cards/Deck").gameObject;
+        // デッキ(山札)から1枚カードを引く
+        yield return null;
+    }
+
+    private IEnumerator RoundAttack()
+    {
+        // フィールドにあるカードで攻撃する
+        yield return null;
+    }
+
+    private IEnumerator RoundCalcDamage()
+    {
+        // 攻撃側なら攻撃したとき、守備側なら攻撃されたときのダメージを計算する
+        yield return null;
+    }
+
+    private IEnumerator RoundTomb()
+    {
+        // HPが0になったカードを墓地に送る
+        // フィールドにいるカード(攻撃、防御、イベント)が、墓地に行く可能性有
+        yield return null;
+    }
+
+    private IEnumerator RoundTurnChange()
+    {
+        tm.changeTurn();
+        yield return null;
+    }
+
+    // 1試合が終わったかどうかを判定する
+    bool isFinished()
+    {
+        return false;
+    }
+}
